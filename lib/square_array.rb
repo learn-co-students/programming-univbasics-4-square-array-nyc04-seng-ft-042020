d788a3bef25a79c2ae9eb8902d97b7b85b8d1dda
Kyle B <kjburda@gmail.com>
2020-02-14 21:44:26 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,19 @@
+# initilizing an array to test
+array = [1, 2, 3]
+
 def square_array(array)
-  # your code here
+
+sq_array = Array.new
+
+i = 0 
+
+  while i < array.size
+    sum = array[i] ** 2
+    sq_array.insert(i, sum)
+    i += 1
+  end
+p sq_array
 end
+
+# calling method to test output
+square_array(array)
